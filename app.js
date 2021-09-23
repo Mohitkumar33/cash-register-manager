404a9887f83ae3dde11af7d24f34c50e4492f64a
Mohit kumar <46507817+Mohitkumar33@users.noreply.github.com>
2021-09-23 17:47:52 +0530

    refactor: added function to calc. the min. notes

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#output-box");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+function calculateBill(amt) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberofNotes = Math.trunc(amt / availableNotes[i]);
+    amt = amt % availableNotes[i];
+    noOfNotes[i].innerText = numberofNotes;
+  }
+}
 
 function showMessage(msg) {
   message.style.display = "block";
@@ -13,6 +24,8 @@ function calculateNotes() {
   if (billAmount.value > 0) {
     if (billAmount.value < cashGiven.value) {
       showMessage("success");
+      const amountToBeReturned = cashGiven.value - billAmount.value;
+      calculateBill(amountToBeReturned);
     } else {
       showMessage("Please give the cash greater than the bill amount");
     }
